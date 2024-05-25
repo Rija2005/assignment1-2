1cab103507906425af870c1a6c6402781ce8b3dc
Rija2005 <syedarija2005@gmail.com>
2024-05-25 10:49:39 -0700

    my assigment

---
+++ script.js
@@ -0,0 +1,76 @@
+  //CHAPTER1
+  //question 1
+//   let greeting = "Hello! ";
+// alert(greeting + "welcome to our website")
+
+
+// //question2
+// let message =  "JavaScript Alert"; 
+// alert(message +  " \nError! Please enter a valid password");
+
+// //question3
+// let text = "Welcome to JS land\n Enjoy Happy Coding!";
+// alert( text)
+
+//questin4
+// let message = "JavaScript Alert";
+// alert(message+ "\n welcome to js land");
+// alert(message+ "\n Happy Coding!\n prevent this from creating additional dialogs")
+
+// //question5
+// let text ="JavaScript Alert"
+// console.log(text+"Hello I can run JS through my web browser console");
+
+//question6
+
+
+
+//question7
+
+
+
+
+//CHAPTER2
+//question1
+// var username="Rija"
+// console.log(username);
+
+// //question2
+
+// var myName = "syeda rija";
+//  console.log(myName);
+
+
+//  //question3
+//  var message = "Hello World";
+//  alert (message);
+
+// //question4
+//  var Name ="John Doe";
+//  alert (Name);
+//  var age ="15 years old";
+//  alert(age);
+//  var text ="Certified Mobile Application Development";
+//  alert(text);
+
+//  //question5
+//  var food ="pizza \n pizz \n piz \n pi \n p";
+//  alert(food);
+
+//  //question6
+//  let email="example@gmail.com";
+//  alert("My email address is" +email);
+
+ //question7
+
+ let book ="A smarter way to learn JavaScript";
+ alert(book);
+
+ //question 8
+
+
+
+ //question9
+
+ let logo = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+ alert(logo);
